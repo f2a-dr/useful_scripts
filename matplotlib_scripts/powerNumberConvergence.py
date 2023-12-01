0c48c6d6a7899fabcef00696c01d25dbbecfdc8a
fderoma <francesco.deroma92@gmail.com>
2023-12-01 16:08:12 +0100

    Added flag laminar to make easier the print in case of laminar and turbulent models

--- matplotlib_scripts/powerNumberConvergence.py
+++ matplotlib_scripts/powerNumberConvergence.py
@@ -3,11 +3,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 #matplotlib.use('Agg')
 
-# List the file to process
-directory = "postProcessing/"
-subdir = ["forces_1/", "forces_2/", "epsilon_avg/", "power_dissipation/"]
-files = ["forces.dat", "forces.dat", "volFieldValue.dat", "volFieldValue.dat"]
-
 # Physical properties needed for the calculation
 rho = 1000
 mu = 1e-3
@@ -20,11 +15,26 @@ T = 0.230
 D = T/3
 Re = rho*N*D**2/mu
 
+laminar = False
+
+# List the file to process
+directory = "postProcessing/"
+if laminar:
+    subdir = ["forces_1/", "forces_2/"]
+    files = ["forces.dat", "forces.dat"]
+else:
+    subdir = ["forces_1/", "forces_2/", "epsilon_avg/", "power_dissipation/"]
+    files = ["forces.dat", "forces.dat", "volFieldValue.dat", "volFieldValue.dat"]
+
 # Specify what will go in each plot
-howToPlot = [[0, 1], [2, 3], [4, 5, 6, 7], [8, 9, 10, 11]]
-# howToPlot = [[0, 1], [2, 3]]
-fileout = ["momenta", "dissipation", "power", "powerNumber"]
-labels = [r"$M_z^\mathrm{stat}$", r"$M_z^\mathrm{rot}$", r"$\bar{\epsilon}$", r"$\frac{\int_{V}\epsilon\,dV}{V}$", r"$P^\mathrm{stat}$", r"$P^\mathrm{rot}$", r"$P^{\bar{\epsilon}}$", r"$P^{\epsilon V}$", r"$N_p^{\mathrm{stat}}$", r"$N_p^{\mathrm{rot}}$", r"$N_p^{\bar{\epsilon}}$", r"$N_p^{\epsilon V}$"]
+if laminar:
+    howToPlot = [[0, 1], [2, 3], [4, 5]]
+    fileout = ["momenta", "power", "powerNumber"]
+    labels = [r"$M_z^\mathrm{stat}$", r"$M_z^\mathrm{rot}$", r"$P^\mathrm{stat}$", r"$P^\mathrm{rot}$", r"$N_p^{\mathrm{stat}}$", r"$N_p^{\mathrm{rot}}$"]
+else:
+    howToPlot = [[0, 1], [2, 3], [4, 5, 6, 7], [8, 9, 10, 11]]
+    fileout = ["momenta", "dissipation", "power", "powerNumber"]
+    labels = [r"$M_z^\mathrm{stat}$", r"$M_z^\mathrm{rot}$", r"$\bar{\epsilon}$", r"$\frac{\int_{V}\epsilon\,dV}{V}$", r"$P^\mathrm{stat}$", r"$P^\mathrm{rot}$", r"$P^{\bar{\epsilon}}$", r"$P^{\epsilon V}$", r"$N_p^{\mathrm{stat}}$", r"$N_p^{\mathrm{rot}}$", r"$N_p^{\bar{\epsilon}}$", r"$N_p^{\epsilon V}$"]
 colors = ["tab:blue", "tab:orange", "tab:green", "tab:red", "tab:purple", "tab:brown", "tab:pink", "tab:grey", "tab:olive", "tab:cyan"]
 
 # Momenta_static    Momenta_rotating    Epsilon_average     Epsilon_integrated
@@ -142,4 +152,5 @@ for i in range(len(howToPlot)):
 print("\n### Power Number ###")
 print("Static parts, Np = {}".format(values[2*len(files)][-1]))
 print("Rotating parts, Np = {}".format(values[1+2*len(files)][-1]))
-# print("Epsilon, Np = {}".format(values[2+2*len(files)][-1]))
+if not(laminar):
+    print("Epsilon, Np = {}".format(values[2+2*len(files)][-1]))
