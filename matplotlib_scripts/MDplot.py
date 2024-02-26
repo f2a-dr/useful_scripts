add31a1d5e79c05dcfd445e297ed173728fb60d3
fderoma <francesco.deroma92@gmail.com>
2024-02-26 10:45:05 +0100

    Fixed error on y axis limit visualization

--- matplotlib_scripts/MDplot.py
+++ matplotlib_scripts/MDplot.py
@@ -57,7 +57,7 @@ for i in range(1, len(header)):
     ax = fig[i].add_subplot(gs[i][0, 0])
     ax.set_xlabel(r"Step")
     ax.set_ylabel(header[i]+"DPD units")
-    ax.set_ylim(np.mean(fullData[i])+10*np.std(fullData[i]), np.mean(fullData[i])-10*np.std(fullData[i]))
+    ax.set_ylim(np.mean(fullData[i])-10*np.std(fullData[i]), np.mean(fullData[i])+10*np.std(fullData[i]))
     ax.plot(fullData[0], fullData[i], color="dodgerblue", label=header[i])
     ax.plot(data[0], np.mean(data[i])*np.ones(len(data[0])), color="crimson", label=r"$\langle$"+header[i]+r"$\rangle$")
     ax.text(0.5, 0.25, "mean = {ave}\nstdv = {stdv}".format(ave=round(np.mean(data[i]),4), stdv=round(np.std(data[i]),4)), ha="center", va="center", fontsize=12, bbox=dict(edgecolor="crimson", facecolor="dodgerblue", alpha=1), color="white", transform=ax.transAxes)
