1ddc7ee84f57b0976959bce507221095b7cf65d1
fderoma <francesco.deroma92@gmail.com>
2024-03-07 10:09:40 +0100

    Updated script for plotting results from MD or DPD simulation, added flag to save figures in files, fixed fontsize of various elements

--- matplotlib_scripts/MDplot.py
+++ matplotlib_scripts/MDplot.py
@@ -19,6 +19,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 #matplotlib.use('Agg')
 
+toSave = False
+
 # Constant parameter definitions
 printTime = 100			# This variable is how often the result are printed in the log.lammps file
 eqTime = 300000			# This variable specify at which timestep you want to consider the equilibration finished (must be a multiple of printTime)
@@ -56,13 +58,18 @@ for i in range(1, len(header)):
     gs[i] = fig[i].add_gridspec(1, 1)
 
     ax = fig[i].add_subplot(gs[i][0, 0])
-    ax.set_xlabel(r"Step")
-    ax.set_ylabel(header[i]+", DPD units")
+    ax.set_xlabel(r"Step", fontsize=16)
+    ax.set_ylabel(header[i]+", DPD units", fontsize=16)
     ax.set_ylim(np.mean(fullData[i])-10*np.std(fullData[i]), np.mean(fullData[i])+10*np.std(fullData[i]))
     ax.plot(fullData[0], fullData[i], color="dodgerblue", label=header[i])
     ax.plot(data[0], np.mean(data[i])*np.ones(len(data[0])), color="crimson", label=r"$\langle$"+header[i]+r"$\rangle$")
-    ax.text(0.5, 0.25, "mean = {ave}\nstdv = {stdv}".format(ave=round(np.mean(data[i]),4), stdv=round(np.std(data[i]),4)), ha="center", va="center", fontsize=12, bbox=dict(edgecolor="crimson", facecolor="dodgerblue", alpha=1), color="white", transform=ax.transAxes)
-    ax.legend()
+    ax.text(0.5, 0.25, "mean = {ave}\nstdv = {stdv}".format(ave=round(np.mean(data[i]),4), stdv=round(np.std(data[i]),4)), ha="center", va="center", fontsize=16, bbox=dict(edgecolor="crimson", facecolor="dodgerblue", alpha=1), color="white", transform=ax.transAxes)
+    ax.tick_params(axis="both", which="major", labelsize=14)
+    ax.legend(fontsize=14)
 
+if toSave:
+    for i in range(1, len(header)):
+        fig[i].savefig(header[i]+".pdf")
+else:
+    plt.show()
 
-plt.show()
