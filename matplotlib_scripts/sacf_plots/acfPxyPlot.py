97e3488b7b919993693ce9cf8377bd9f4b20940d
fderoma <francesco.deroma92@gmail.com>
2024-12-02 20:13:32 +0100

    updated plot for Pxy autocorrelation function

--- matplotlib_scripts/sacf_plots/acfPxyPlot.py
+++ matplotlib_scripts/sacf_plots/acfPxyPlot.py
@@ -18,7 +18,7 @@ with open(filename) as f:
     info = info.strip().split()
     n = int(info[1])
 
-with open('tmp.txt', 'w') as fw:
+with open('tmp.dat', 'w') as fw:
     subprocess.call(['tail', '-n', str(n), filename], stdout=fw)
 
 with open('tmp.dat') as f:
@@ -27,7 +27,7 @@ with open('tmp.dat') as f:
         time.append(0.01*float(line[1]))
         pxy.append(float(line[3]))
 
-subprocess.run(['rm', 'tmp.txt'])
+subprocess.run(['rm', 'tmp.dat'])
 
 # Plot
 plt.rcParams.update({
@@ -40,10 +40,10 @@ centimeters = 1/2.54
 fig = plt.figure(figsize=(10*centimeters, 10*centimeters), constrained_layout=True)
 gs = fig.add_gridspec(1, 1)
 
-ax = fig1.add_subplot(gs[0, 0])
-ax.set_xlabel(r'Time, (-)', fontsize=16)
-ax.set_ylabel(r'$\langle P_{xy}P_{xy}\rangle$', fontsize=16)
-ax.plot(time, np.zeros(len(time)), color=black)
+ax = fig.add_subplot(gs[0, 0])
+ax.set_xlabel(r'Time, $(-)$', fontsize=16)
+ax.set_ylabel(r'$\langle P_{xy}P_{xy}\rangle,\ (-)$', fontsize=16)
+ax.plot(time, np.zeros(len(time)), color='black')
 ax.plot(time, pxy, linewidth=0.5, marker='o', markersize=2, color='skyblue')
 
 fig.savefig('autoCorrPxy.' + saveFormat, dpi=300)
